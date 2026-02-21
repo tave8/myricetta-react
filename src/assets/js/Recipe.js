1acf3c89d4e328d8b61df7e1288ae93bf9caee49
Giuseppe Tavella <giuseppetavella8@gmail.com>
2026-02-21 17:14:34 +0100

    ?

--- src/assets/js/Recipe.js
+++ src/assets/js/Recipe.js
@@ -242,8 +242,10 @@ class Recipe {
   existsIngredientByName(ingredientName) {
     const item = this.getIngredientByName(ingredientName)
     console.log("in existsIngredientByName")
-    console.log(item)
-    console.log(item.constructor.name)
+    if (item) {
+      console.log(item)
+      console.log(item.constructor.name)
+    }
     const exists = item !== undefined && item.constructor.name === "Ingredient"
     return exists
   }
