a15a9e94ca23d6f5ac4c0b8da535286c06b04a9e
Giuseppe Tavella <giuseppetavella8@gmail.com>
2026-02-21 15:08:37 +0100

    from one ingrediente, calculate others: show ingredients list

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -9,23 +9,28 @@ const RecipeComponent = (props) => {
   const [_recipeInstance, _setRecipeInstance] = useState(new Recipe())
   // recipe info
   const [recipeName, setRecipeName] = useState("")
-  // ingredients
+  // initial/original ingredients
   const [ingredientsCalculations, setIngredientsCalculations] = useState(null)
+  // ingredients from 1 ingredient
+  const [ingredientsCalculationsFromIngredient, setIngredientsCalculationsFromIngredient] = useState(null)
   // the new ingredient that is being added
   const [newIngredientName, setNewIngredientName] = useState("")
   const [newIngredientQuantity, setNewIngredientQuantity] = useState(1)
+  // the one ingredient to calculate the others from
+  const [knownIngredientName, setKnownIngredientName] = useState("")
+  const [knownIngredientQuantity, setKnownIngredientQuantity] = useState(1)
 
   return (
     // centers content in the page
     <Row className="justify-content-center">
       <Col xs={12} md={6}>
         <Row className="flex-column g-3">
-          {/* TITLE */}
-          <Col className="">
+          {/* PAGE TITLE */}
+          <Col>
             <h2 className="text-center">Aggiungi ricetta</h2>
           </Col>
           {/* RECIPE INFO (name, photo etc.) */}
-          <Col className="">
+          <Col>
             <Form.Group>
               <Form.Control
                 type="text"
@@ -38,109 +43,31 @@ const RecipeComponent = (props) => {
               />
             </Form.Group>
           </Col>
-          {/* ADD INGREDIENT */}
-          <Col className="">
-            <Row>
-              {/* new ingredient name */}
-              <Col xs={6}>
-                <Form.Group>
-                  <Form.Control
-                    type="text"
-                    placeholder="Ingrediente"
-                    id="new-ingredient-name"
-                    value={newIngredientName}
-                    onChange={(event) => {
-                      const val = event.target.value
-                      setNewIngredientName(val)
-                    }}
-                    onKeyDown={(event) => {
-                      // if user has typed Enter, trigger add ingredient
-                      if (event.key == "Enter") {
-                        const context = {
-                          setNewIngredientName,
-                          setNewIngredientQuantity,
-                          newIngredientName,
-                          newIngredientQuantity,
-                          setIngredientsCalculations,
-                          _recipeInstance,
-                        }
-                        addIngredientHelper(context)()
-                      }
-                    }}
-                  />
-                </Form.Group>
-              </Col>
-              {/* new ingredient quantity  */}
-              <Col xs={3}>
-                <Form.Group>
-                  <Form.Control
-                    type="number"
-                    placeholder="Quantità (g)"
-                    value={newIngredientQuantity}
-                    onChange={(event) => {
-                      const val = event.target.value
-                      setNewIngredientQuantity(val)
-                    }}
-                    onKeyDown={(event) => {
-                      // if user has typed Enter, trigger add ingredient
-                      if (event.key == "Enter") {
-                        const context = {
-                          setNewIngredientName,
-                          setNewIngredientQuantity,
-                          newIngredientName,
-                          newIngredientQuantity,
-                          setIngredientsCalculations,
-                          _recipeInstance,
-                        }
-                        addIngredientHelper(context)()
-                      }
-                    }}
-                  />
-                </Form.Group>
-              </Col>
-              <Col xs={3} className="text-end">
-                <Button
-                  variant="primary"
-                  onClick={() => {
-                    const context = {
-                      setNewIngredientName,
-                      setNewIngredientQuantity,
-                      newIngredientName,
-                      newIngredientQuantity,
-                      setIngredientsCalculations,
-                      _recipeInstance,
-                    }
-                    addIngredientHelper(context)()
-                  }}
-                >
-                  +
-                </Button>
-              </Col>
-            </Row>
-          </Col>
-          {/* INGREDIENTS LIST (proportions etc.) */}
+          {/* INGREDIENTS (add ingredient, ingredients list) */}
           <Col>
-            <Table striped bordered hover>
-              <thead>
-                <tr>
-                  <th>Ingr.</th>
-                  <th>Q.tà</th>
-                  <th>%</th>
-                  <th></th>
-                </tr>
-              </thead>
-              <tbody>
-                {/* exist ingredients */}
-                {ingredientsCalculations?.ingredients.map((ingredient) => {
-                  return (
-                    <tr key={ingredient.id}>
-                      <td>{ingredient.name}</td>
-                      <td>{ingredient.quantityRounded}</td>
-                      <td>{ingredient.percentageRounded}</td>
-                      <td className="text-end">
-                        <Button
-                          variant="danger"
-                          onClick={() => {
+            <Row className="flex-column g-3">
+              {/* title */}
+              <Col>
+                <h4>Ingredienti</h4>
+              </Col>
+              {/* add ingredient */}
+              <Col>
+                <Row>
+                  {/* new ingredient name */}
+                  <Col xs={6}>
+                    <Form.Group>
+                      <Form.Control
+                        type="text"
+                        placeholder="Ingrediente"
+                        id="new-ingredient-name"
+                        value={newIngredientName}
+                        onChange={(event) => {
+                          const val = event.target.value
+                          setNewIngredientName(val)
+                        }}
+                        onKeyDown={(event) => {
+                          // if user has typed Enter, trigger add ingredient
+                          if (event.key == "Enter") {
                             const context = {
                               setNewIngredientName,
                               setNewIngredientQuantity,
@@ -149,32 +76,202 @@ const RecipeComponent = (props) => {
                               setIngredientsCalculations,
                               _recipeInstance,
                             }
-                            const ingredientNameToRemove = ingredient.name
-                            removeIngredientHelper(context)(ingredientNameToRemove)
-                          }}
-                        >
-                          -
-                        </Button>
-                      </td>
+                            addIngredientHelper(context)()
+                          }
+                        }}
+                      />
+                    </Form.Group>
+                  </Col>
+                  {/* new ingredient quantity  */}
+                  <Col xs={3}>
+                    <Form.Group>
+                      <Form.Control
+                        type="number"
+                        placeholder="Quantità (g)"
+                        value={newIngredientQuantity}
+                        onChange={(event) => {
+                          const val = event.target.value
+                          setNewIngredientQuantity(val)
+                        }}
+                        onKeyDown={(event) => {
+                          // if user has typed Enter, trigger add ingredient
+                          if (event.key == "Enter") {
+                            const context = {
+                              setNewIngredientName,
+                              setNewIngredientQuantity,
+                              newIngredientName,
+                              newIngredientQuantity,
+                              setIngredientsCalculations,
+                              _recipeInstance,
+                            }
+                            addIngredientHelper(context)()
+                          }
+                        }}
+                      />
+                    </Form.Group>
+                  </Col>
+                  <Col xs={3} className="text-end">
+                    <Button
+                      variant="primary"
+                      onClick={() => {
+                        const context = {
+                          setNewIngredientName,
+                          setNewIngredientQuantity,
+                          newIngredientName,
+                          newIngredientQuantity,
+                          setIngredientsCalculations,
+                          _recipeInstance,
+                        }
+                        addIngredientHelper(context)()
+                      }}
+                    >
+                      +
+                    </Button>
+                  </Col>
+                </Row>
+              </Col>
+              {/* ingredients list */}
+              <Col>
+                <Table striped bordered hover>
+                  <thead>
+                    <tr>
+                      <th>Ingr.</th>
+                      <th>Q.tà</th>
+                      <th>%</th>
+                      <th></th>
                     </tr>
-                  )
-                })}
-                {/* no calculation yet */}
-                {ingredientsCalculations?.ingredients.length == 0 && (
-                  <tr>
-                    <td colSpan="4">Aggiungi il primo ingrediente</td>
-                  </tr>
-                )}
-              </tbody>
-              <tfoot>
-                {ingredientsCalculations?.ingredients.length > 0 && (
-                  <tr>
-                    <td>TOTALE:</td>
-                    <td>{ingredientsCalculations.totIngredientsRounded}</td>
-                  </tr>
-                )}
-              </tfoot>
-            </Table>
+                  </thead>
+                  <tbody>
+                    {/* exist ingredients */}
+                    {ingredientsCalculations?.ingredients.map((ingredient) => {
+                      return (
+                        <tr key={ingredient.id}>
+                          <td>{ingredient.name}</td>
+                          <td>{ingredient.quantityRounded}</td>
+                          <td>{ingredient.percentageRounded}</td>
+                          <td className="text-end">
+                            <Button
+                              variant="danger"
+                              onClick={() => {
+                                const context = {
+                                  setNewIngredientName,
+                                  setNewIngredientQuantity,
+                                  newIngredientName,
+                                  newIngredientQuantity,
+                                  setIngredientsCalculations,
+                                  _recipeInstance,
+                                }
+                                const ingredientNameToRemove = ingredient.name
+                                removeIngredientHelper(context)(ingredientNameToRemove)
+                              }}
+                            >
+                              -
+                            </Button>
+                          </td>
+                        </tr>
+                      )
+                    })}
+                    {/* no calculation yet */}
+                    {ingredientsCalculations?.ingredients.length == 0 && (
+                      <tr>
+                        <td colSpan="4">Aggiungi il primo ingrediente</td>
+                      </tr>
+                    )}
+                  </tbody>
+                  <tfoot>
+                    {ingredientsCalculations?.ingredients.length > 0 && (
+                      <tr>
+                        <td>TOTALE:</td>
+                        <td>{ingredientsCalculations.totIngredientsRounded}</td>
+                      </tr>
+                    )}
+                  </tfoot>
+                </Table>
+              </Col>
+            </Row>
+          </Col>
+          {/* FROM 1 INGREDIENT, CALCULATE THE OTHERS */}
+          <Col>
+            <Row className="flex-column g-3">
+              {/* title */}
+              <Col>
+                <h4>Ho un ingrediente, calcola gli altri</h4>
+              </Col>
+              {/* known ingredient (name, quantity) */}
+              <Col>
+                <Row>
+                  {/* known ingredient quantity */}
+                  <Col xs={4}>
+                    <Form.Group>
+                      <Form.Control
+                        type="number"
+                        placeholder="Quantità (g)"
+                        value={knownIngredientQuantity}
+                        onChange={(event) => {
+                          const val = event.target.value
+                          setKnownIngredientQuantity(val)
+                        }}
+                      />
+                    </Form.Group>
+                  </Col>
+                  {/* known ingredient name */}
+                  <Col xs={8}>
+                    <Form.Group controlId="exampleSelect">
+                      <Form.Select defaultValue={""}>
+                        <option value="">
+                          Seleziona ingrediente
+                        </option>
+                        {ingredientsCalculations?.ingredients.map((ingredient, i) => {
+                          return (
+                            <option key={i} value={ingredient.name}>
+                              {ingredient.name}
+                            </option>
+                          )
+                        })}
+                      </Form.Select>
+                    </Form.Group>
+                  </Col>
+                </Row>
+              </Col>
+              {/* ingredients list */}
+              <Col>
+                <Table striped bordered hover>
+                  <thead>
+                    <tr>
+                      <th>Ingr.</th>
+                      <th>Q.tà</th>
+                      <th>%</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {/* exist ingredients */}
+                    {ingredientsCalculationsFromIngredient?.ingredients.map((ingredient) => {
+                      return (
+                        <tr key={ingredient.id}>
+                          <td>{ingredient.name}</td>
+                          <td>{ingredient.quantityRounded}</td>
+                          <td>{ingredient.percentageRounded}</td>
+                        </tr>
+                      )
+                    })}
+                    {/* no calculation yet */}
+                    {ingredientsCalculationsFromIngredient?.ingredients.length == 0 && (
+                      <tr>
+                        <td colSpan="4">Aggiungi il primo ingrediente</td>
+                      </tr>
+                    )}
+                  </tbody>
+                  <tfoot>
+                    {ingredientsCalculationsFromIngredient?.ingredients.length > 0 && (
+                      <tr>
+                        <td>TOTALE:</td>
+                        <td>{ingredientsCalculationsFromIngredient.totIngredientsRounded}</td>
+                      </tr>
+                    )}
+                  </tfoot>
+                </Table>
+              </Col>
+            </Row>
           </Col>
         </Row>
       </Col>
