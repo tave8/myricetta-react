3d90c349bbfcd869f673d7a4a2a4343b21b8f8be
Giuseppe Tavella <giuseppetavella8@gmail.com>
2026-02-21 09:48:03 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import "./App.css"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+function App() {
+  return <></>
+}
+
+export default App
