2b2d92f4eaab890353f3002f8698b6edc084ddc2
Giuseppe Tavella <giuseppetavella8@gmail.com>
2026-02-28 17:13:14 +0100

    navbar close automatic when link click

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,36 +6,29 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Recipe from "../assets/js/Recipe/Recipe"
 
 const NavbarComponent = (props) => {
+  const [expanded, setExpanded] = useState(false)
+
+  const closeNavbar = () => setExpanded(false)
+
   return (
     <Col xs={12} className="p-0" style={{}}>
-      <Navbar expand="lg" className="bg-body-tertiary">
+      <Navbar expand="lg" expanded={expanded} onToggle={setExpanded} className="bg-body-tertiary">
         <Container fluid>
-          <Navbar.Brand as={Link} to="/add-recipe">
+          <Navbar.Brand as={Link} to="/add-recipe" onClick={closeNavbar}>
             myricetta
           </Navbar.Brand>
+
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link as={Link} to="/my-recipes">
+              <Nav.Link as={Link} to="/my-recipes" onClick={closeNavbar}>
                 Le mie ricette
               </Nav.Link>
-              <Nav.Link as={Link} to="/add-recipe">
+
+              <Nav.Link as={Link} to="/add-recipe" onClick={closeNavbar}>
                 Aggiungi ricetta
               </Nav.Link>
-              {/* <Nav.Link as={Link} to="/see-recipes">
-                        Le mie ricette
-                      </Nav.Link> */}
-
-              {/* <Nav.Link href="#home">Home</Nav.Link>
-                      <Nav.Link href="#link">Link</Nav.Link> */}
-
-              {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                      </NavDropdown> */}
             </Nav>
           </Navbar.Collapse>
         </Container>
