41924e1b334ee20ed22ff16890fc1ed9992b35dc
Giuseppe Tavella <giuseppetavella8@gmail.com>
2026-03-01 12:09:47 +0100

    navbar with icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,39 +1,63 @@
 import { useState } from "react"
 import { Container, Row, Col, CardGroup, Card, Spinner, Alert, Button, Image, Form, ListGroup, Navbar, NavDropdown, Nav } from "react-bootstrap"
+import { ListUl as ListUlIcon, PlusCircleFill as PlusCircleFillIcon } from "react-bootstrap-icons"
 import { Helmet } from "react-helmet"
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 
 import Recipe from "../assets/js/Recipe/Recipe"
 
 const NavbarComponent = (props) => {
-  const [expanded, setExpanded] = useState(false)
-
-  const closeNavbar = () => setExpanded(false)
-
   return (
-    <Col xs={12} className="p-0" style={{}}>
-      <Navbar expand="lg" expanded={expanded} onToggle={setExpanded} className="bg-body-tertiary">
-        <Container fluid>
-          <Navbar.Brand as={Link} to="/my-recipes" onClick={closeNavbar}>
-            myricetta
-          </Navbar.Brand>
+    <>
+      {/* MOBILE */}
+      <Col xs={12} className="p-0">
+        <Navbar className="bg-body-tertiary">
+          <Container fluid>
+            <Navbar.Brand as={Link} to="/my-recipes">
+              myricetta
+            </Navbar.Brand>
 
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto ms-3 gap-4">
+                {/* my recipes */}
+                <Nav.Link as={Link} to="/my-recipes">
+                  <ListUlIcon size={30} />
+                </Nav.Link>
 
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link as={Link} to="/my-recipes" onClick={closeNavbar}>
-                Le mie ricette
-              </Nav.Link>
+                {/* add recipe */}
+                <Nav.Link as={Link} to="/add-recipe">
+                  <PlusCircleFillIcon size={30} />
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </Col>
+      {/* DESKTOP */}
+      {/* <Col xs={12} className="d-none d-md-block p-0">
+        <Navbar expand="lg" expanded={expanded} onToggle={setExpanded} className="bg-body-tertiary">
+          <Container fluid>
+            <Navbar.Brand as={Link} to="/my-recipes" onClick={closeNavbar}>
+              myricetta
+            </Navbar.Brand>
 
-              <Nav.Link as={Link} to="/add-recipe" onClick={closeNavbar}>
-                Aggiungi ricetta
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </Col>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link as={Link} to="/my-recipes" onClick={closeNavbar}>
+                  Le mie ricette
+                </Nav.Link>
+
+                <Nav.Link as={Link} to="/add-recipe" onClick={closeNavbar}>
+                  Aggiungi ricetta
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </Col> */}
+    </>
   )
 }
 
